bc194fbe28658b577dc4d013648c923bd2330600
mr-erold <mmpterold@gmail.com>
2022-08-13 15:13:20 +0200

    Initial commit

--- src/test/java/com/erold/java/yactl/AppTest.java
+++ src/test/java/com/erold/java/yactl/AppTest.java
@@ -1,9 +1,16 @@
 package com.erold.java.yactl;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
 }
