65719d249f88c8bebb0d093c73605745ebd38d06
mr-erold <mmpterold@gmail.com>
2022-08-14 12:33:18 +0200

    Add the basic constants definitions

---
+++ src/main/java/com/erold/java/yactl/constant/Constants.java
@@ -0,0 +1,34 @@
+package com.erold.java.yactl.constant;
+
+public class Constants {
+    // Border onstants
+
+    // Double border constants.
+
+    public static final String BORDER_D_HORI = "═";
+    public static final String BORDER_D_VERT = "║";
+    public static final String BORDER_D_TOPLEFT = "╔";
+    public static final String BORDER_D_TOPRIGHT = "╗";
+    public static final String BORDER_D_BOTTOMLEFT = "╚";
+    public static final String BORDER_D_BOTTOMRIGHT = "╝";
+    public static final String BORDER_D_LEFTEDGE = "╠";
+    public static final String BORDER_D_RIGHTEDGE = "╣";
+    public static final String BORDER_D_TOPEDGE = "╦";
+    public static final String BORDER_D_BOTTOMEDGE = "╩";
+    public static final String BORDER_D_MIDDLE = "╬";
+
+    // Single border constants.
+
+    public static final String BORDER_S_HORI = "─";
+    public static final String BORDER_S_VERT = "│";
+    public static final String BORDER_S_TOPLEFT = "┌";
+    public static final String BORDER_S_TOPRIGHT = "┐";
+    public static final String BORDER_S_BOTTOMLEFT = "└";
+    public static final String BORDER_S_BOTTOMRIGHT = "┘";
+    public static final String BORDER_S_LEFTEDGE = "├";
+    public static final String BORDER_S_RIGHTEDGE = "┤";
+    public static final String BORDER_S_TOPEDGE = "┬";
+    public static final String BORDER_S_BOTTOMEDGE = "┴";
+    public static final String BORDER_S_MIDDLE = "┼";
+
+}
